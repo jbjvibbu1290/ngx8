e8d07f15f828749a0239a7383b95ae48641beb1a
rakesh.k <rakeshkattamudi2@gmail.com>
2021-12-20 22:35:30 +0530

    final-fixes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {TranslateService} from '@ngx-translate/core';
+import {_} from '@biesbjerg/ngx-translate-extract/dist/utils/utils';
+
+import defaultLanguage from './../assets/i18n/en.json';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ngx-demo';
+
+    fromCode = _('demo.text-in-code');
+
+    constructor(private translate: TranslateService) {
+        translate.setTranslation('en', defaultLanguage);
+        translate.setDefaultLang('en');
+    }
+
+    useLanguage(language: string) {
+        this.translate.use(language);
+    }
 }
